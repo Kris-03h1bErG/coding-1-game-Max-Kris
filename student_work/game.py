231341aabd41ebc1493016f14048ff522d120303
Kris-03h1bErG <s1007469@student.knoxschools.org>
2026-02-20 15:25:09 +0000

    basic setup

--- student_work/game.py
+++ student_work/game.py
@@ -1,12 +1,15 @@
 # Write your game here
-
+import curses
+import time
 game_data = {
     # Store board dimensions, player/enemy positions, score, energy, collectibles, and icons
 }
 
-def draw_board(screen):
-    # Print the board and all game elements using curses
-
-time.sleep(1)
-print("hi")
-# Good Luck!
+def draw_board():
+    for i in range(15):
+        print("H E L L O W O R L D H E L L O")
+    print("Random stats")
+    print("Random stats")
+    print("Random stats")
+# time.sleep(2)
+draw_board()

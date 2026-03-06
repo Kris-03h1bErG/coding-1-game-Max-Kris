f6108773aefd81e7842462c7c82cf5fffa0d1533
Kris-03h1bErG <s1007469@student.knoxschools.org>
2026-03-06 15:01:49 +0000

    added reset dict def

--- student_work/game.py
+++ student_work/game.py
@@ -69,7 +69,7 @@ def move_player(key):
     game_data['player']['y'] = new_y
 
     if any(o['x'] == new_x and o['y'] == new_y for o in game_data['pipes']):
-        break
+        exit()
 
 def draw_board(stdscr):
     curses.start_color() # initialize requirements for color
@@ -123,3 +123,39 @@ def run_game(stdscr):
             time.sleep(.2)
             
 curses.wrapper(run_game)
+
+def reset_dict(dict)
+    dict = {
+    # Store board dimensions, player/enemy positions, score, energy, collectibles, and icons
+    'width': 7,
+    'height': 10,
+    'player': {"x": 2, "y": 5, "score": 0, "seed" : random.randint(1,100)} , 
+
+    'pipes': [
+        {"x": 7, "y": 0},
+        {"x": 7, "y": 1},
+        {"x": 7, "y": 2},
+        {"x": 7, "y": 3},
+        {"x": 7, "y": 7},
+        {"x": 7, "y": 8},
+        {"x": 7, "y": 9},
+        {"x": 7, "y": 10}
+    ],
+
+    #ASCII icons
+        # Blocks
+    'pipe': "\U0001F7E9", #weird green 
+    'sky' : " ", #"sky": "\U0001F7E6",  #weird blue 
+    "empty":  "Lorsum Ipsum idk?", # this is empty for now 
+
+        # Random Players just a large dict of emojis for player 
+    "icons": {
+            'japan': "\U0001F5FE",
+            'hotel': "\U0001F3E8", 
+            "bball": "\U0001F3C0", 
+            "caution_sign": "\U0001F6A7", 
+            #"pencil": "\u270F",  for some reason is slightly shorter than the rest?
+            "palette": "\U0001F3A8", 
+            "top_hat": "\U0001F3A9",
+        }
+}

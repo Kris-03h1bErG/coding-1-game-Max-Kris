d94f401123324510020410f41e41b95be3fdf2c9
Kris-03h1bErG <s1007469@student.knoxschools.org>
2026-03-20 09:11:31 +0000

    WORKING LOOPING SUDO RANDOM PIPES

--- student_work/game.py
+++ student_work/game.py
@@ -8,17 +8,18 @@ game_data = {
     # Store board dimensions, player/enemy positions, score, energy, collectibles, and icons
     'width': 7,
     'height': 10,
+        'pipes_order' : [   
+                    [   [random.randint(0,4)] , [random.randint(0,4)] , [random.randint(0,4)] , [random.randint(0,4)]],
+                    [   [random.randint(0,4)] , [random.randint(0,4)] , [random.randint(0,4)] , [random.randint(0,4)]],
+                    [   [random.randint(0,4)] , [random.randint(0,4)] , [random.randint(0,4)] , [random.randint(0,4)]], 
+                    [   [random.randint(0,4)] , [random.randint(0,4)] , [random.randint(0,4)] , [random.randint(0,4)]],
+                    [   [random.randint(0,4)] , [random.randint(0,4)] , [random.randint(0,4)] , [random.randint(0,4)]]
+                    ], 
+    'order_list' : [ random.randint(0,4) , random.randint(0,4) , random.randint(0,4) , random.randint(0,4) , random.randint(0,4)],
+    'current_order' : random.randint(0,4),
+    'current_pipe': random.randint(0,4),
+    'next_pipe' : [0, 0], 
     'player': {"x": 2, "y": 5, "score": 0, "seed" : random.randint(1,100)} , 
-    'pipe_seed' : random.randint(0,4),
-    'pipes_order' : [   [random.randint(0,4) , random.randint(0,4) , random.randint(0,4) , random.randint(0,4)],
-                        [random.randint(0,4) , random.randint(0,4) , random.randint(0,4) , random.randint(0,4)],
-                        [random.randint(0,4) , random.randint(0,4) , random.randint(0,4) , random.randint(0,4)], 
-                        [random.randint(0,4) , random.randint(0,4) , random.randint(0,4) , random.randint(0,4)],
-                        [random.randint(0,4) , random.randint(0,4) , random.randint(0,4) , random.randint(0,4)] 
-                    ]
-    'next_order' : [ random.randint(0,4) , random.randint(0,4) , random.randint(0,4) , random.randint(0,4) ]
-    'current_pipe' : 
-    'next_pipe': 
     'pipes': [ 
     [
         {"x": 7, "y": 0}, # normal 
@@ -95,50 +96,6 @@ game_data = {
         }
 
 }
-# def reset_dict(dict):
-#     dict = {
-#     # Store board dimensions, player/enemy positions, score, energy, collectibles, and icons
-#     'width': 7,
-#     'height': 10,
-#     'player': {"x": 2, "y": 5, "score": 0, "seed" : random.randint(1,100)} , 
-
-#     'pipes': [
-#         {"x": 7, "y": 0},
-#         {"x": 7, "y": 1},
-#         {"x": 7, "y": 2},
-#         {"x": 7, "y": 3},
-#         {"x": 7, "y": 7},
-#         {"x": 7, "y": 8},
-#         {"x": 7, "y": 9},
-#         {"x": 7, "y": 10}
-#     ],
-
-#     #ASCII icons
-#         # Blocks
-#     'pipe': "\U0001F7E9", #weird green 
-#     'sky' : " ", #"sky": "\U0001F7E6",  #weird blue 
-#     "empty":  "Lorsum Ipsum idk?", # this is empty for now 
-
-#         # Random Players just a large dict of emojis for player 
-#     "icons": {
-#             'japan': "\U0001F5FE",
-#             'hotel': "\U0001F3E8", 
-#             "bball": "\U0001F3C0", 
-#             "caution_sign": "\U0001F6A7", 
-#             #"pencil": "\u270F",  for some reason is slightly shorter than the rest?
-#             "palette": "\U0001F3A8", 
-#             "top_hat": "\U0001F3A9",
-#         }
-#     }
-
-# def player_died():
-#     global game_data
-#     game_data = reset_dict(game_data)
-#     time.sleep(2)
-#     curses.wrapper(run_game)
-
-
-
 
 def random_player(icon_list , number):
     random.seed(number)
@@ -184,7 +141,7 @@ def draw_board(stdscr):
                 row += random_player(game_data["icons"], game_data["player"]["seed"]) #then  adds player icon to str
                 # Space in code for readability
             # Pipes                                                         #need to get same pipe each time 
-            elif any(p['x'] == x and p['y'] == y for p in game_data['pipes'][game_data['pipe_seed']]):
+            elif any(p['x'] == x and p['y'] == y for p in game_data['pipes'][game_data['next_pipe'][1]]):
                 row += game_data['pipe']
             # Blue sky stuff (I don't think this is actually used)
             else: # need to add more elif's to display more 
@@ -194,18 +151,34 @@ def draw_board(stdscr):
 
     stdscr.refresh() 
 def pipe_movement():
-    curent_pipes = game_data["pipes"][game_data["pipe_seed"]]
+    curent_pipes = game_data["pipes"][game_data["next_pipe"][1]]
     if curent_pipes[0]["x"] > 0:
         for p in curent_pipes:
             p['x'] -= 1
     else:
         for p in curent_pipes:
             p['x'] = 7
-    
+        pick_next_pipe( game_data['order_list'] , game_data['pipes_order'] , game_data['current_order'], game_data['current_pipe'])    
+        #pick_next_pipe(game_data['pipe_seed'] , game_data['order_list'] , game_data['pipes_order'] , game_data['current_order'], ['current_pipe'])
         
     # random.seed(random.randint(1,1000))
     # game_data['pipe_seed'] = random.randint(0,4)
 
+# def pick_next_pipe(starting_position_in_pol , pipe_order_list , pipes_order_in_list , position_in_pol, position_in_poll):
+#     pipe_order_list[starting_position_in_pol]
+
+def pick_next_pipe( pipe_order_list , pipes_order_in_list , position_in_pol, position_in_poll):
+    current_poll = pipe_order_list[position_in_pol]
+    position_in_poll += 1
+    if position_in_pol > 3:
+        position_in_pol = 0
+        position_in_pol = (position_in_pol + 1) % 5
+        position_in_pol += 1
+    game_data["current_order"] = position_in_pol
+    game_data["current_pipe"] = position_in_poll
+    game_data["next_pipe"] = [position_in_pol, position_in_poll]
+
+    
 
 def run_game(stdscr):
     curses.curs_set(0) # sets cursor to invisible 
@@ -224,7 +197,7 @@ def run_game(stdscr):
             move_player(key)
             draw_board(stdscr)
             pipe_movement()
-            if any(o['x'] == game_data["player"]["x"] and o['y'] == game_data["player"]["y"] for o in game_data['pipes'][game_data['pipe_seed']]) or game_data["player"]["y"] == 10:
+            if any(o['x'] == game_data["player"]["x"] and o['y'] == game_data["player"]["y"] for o in game_data['pipes'][game_data['next_pipe'][1]]) or game_data["player"]["y"] == 10:
                 break
             time.sleep(.2)
             
